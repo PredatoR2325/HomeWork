a130a6881991fe174b6cef5019e647815cd2e295
PredatoR2325 <35430161+PredatoR2325@users.noreply.github.com>
2019-11-01 21:44:57 +0300

    Create Ex8.cs

---
+++ 1State/Ex8.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a=0, b=1, value;
+            Console.WriteLine("Введите номер ч-ла фибаначи:");
+            value = Convert.ToInt32(Console.ReadLine());
+            while(value <=0)
+            {
+                Console.WriteLine("ОШИБКА, число должно быть больше 0: ");
+                value = Convert.ToInt32(Console.ReadLine());
+            }
+
+            while(value !=0)
+            {
+                a = a + b;
+                b = a - b;
+                value--;
+                if(value==0)Console.WriteLine(a + " ");
+            }
+        }
+        static int Fibanachi(int i)
+        {
+            //Console.WriteLine(i);
+            if (i < 1) return 0;
+            if (i == 1) return 1;
+            return Fibanachi(i - 1) + Fibanachi(i - 2);
+
+        }
+    }
+}
+
+
